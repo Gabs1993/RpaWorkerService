3825682009bd82c45719c77bd83c445e5c00b3ee
Gabriel Conceição dos Santos <gabrielcsantos1993@gmail.com>
2026-04-21 10:45:09 -0300

    implementando a domain

---
+++ Domain/Entities/CollectedData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class CollectedData
+    {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public string Source { get; private set; } = string.Empty;
+        public string Title { get; private set; } = string.Empty;
+        public string Value { get; private set; } = string.Empty;
+        public DateTime CollectedAt { get; private set; } = DateTime.UtcNow;
+
+        private CollectedData() { }
+
+        public CollectedData(string source, string title, string value)
+        {
+            Source = source;
+            Title = title;
+            Value = value;
+            CollectedAt = DateTime.UtcNow;
+        }
+    }
+}
---
+++ Domain/Interfaces/ICollectedDataRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface ICollectedDataRepository
+    {
+        Task AddAsync(CollectedData data, CancellationToken cancellationToken = default);
+        Task<List<CollectedData>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<CollectedData?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    }
+}
